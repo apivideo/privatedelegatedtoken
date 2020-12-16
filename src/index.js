c339a52c566f82ef009a1fc890ef91cf429d1e89
dougsillars <doug.sillars@gmail.com>
2020-12-16 09:38:51 +0000

    initial working commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.set('view engine','pug');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+var request = require("request");
 const Discord = require('discord.js');
 const discordClient = new Discord.Client();
 // when the client is ready, run this code
@@ -20,10 +20,7 @@ discordClient.once('ready', () => {
 
 });
 
-// login to Discord with your app's token
-//ginstructions for a discord API toke:
-//https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot
-discordClient.login(process.env.discordKey);
+
 
 
 
@@ -41,7 +38,95 @@ app.get('/', (req, res) => {
 });
 
 
+app.post('/createVideo', (req,res) => {
+	console.log("request body",req.body);
+	//now we'll make 2 rewquests to apivideo
+	//1 create a videoId for the uppload, with any desired params
+	//2 create a delegated token for the upload
+	var public = true;
+	if(req.body.public === "false"){
+		public=false;
+	}
+	var mp4  = true;
+	if(req.body.mp4Support === "false"){
+		mp4=false;
 
+	}
+	var title = req.body.title;
+	var descr = req.body.description;
+
+	client = new apiVideo.Client({ apiKey: apiVideoKey});	
+	
+	
+	let result = client.videos.create(title, {	"title": title, "mp4Support": mp4,
+		"public": public, 
+		"description": descr,					
+	});
+	console.log(result);
+	result.then(function(video) {
+		console.log(video);
+		var videoId = video.videoId;
+		console.log(videoId);
+		//ok have a new videoId for the video - now create a delegated token
+		//since the new delegated token with TTL is not yet in the Node SDK, I'll have to authenticate 
+		//and then request a token - 2 calls to api.video
+		var authOptions = {
+			method: 'POST',
+			url: 'https://ws.api.video/auth/api-key',
+			headers: {
+				accept: 'application/json'
+				
+			},
+			json: {"apiKey":apiVideoKey}
+
+		}
+		console.log(authOptions);	
+		request(authOptions, function (error, response, body) {
+			if (error) throw new Error(error);
+			//this will give me the api key
+			
+			var authToken = body.access_token;
+			console.log(authToken);
+			//now use this to generate a delegated toke with a ttl of 90s
+			var tokenTTL = 90;
+			var tokenOptions = {
+				method: 'POST',
+				url: 'https://ws.api.video/upload-tokens',
+				headers: {
+					accept: 'application/json',
+					authorization: 'Bearer ' +authToken
+				},
+				json: {"ttl":tokenTTL}
+	
+			}
+			request(tokenOptions, function (error, response, body) {
+				if (error) throw new Error(error);
+				var delegatedToken = body.token;
+				var tokenExpiry = body.expiresAt;
+				console.log("new token", delegatedToken);
+				console.log("new token expires", tokenExpiry);
+				var tokenVideoIdJson = {"token": delegatedToken,
+										"expires":tokenExpiry,
+										"videoId": videoId};
+				res.setHeader('Content-Type', 'application/json');
+				res.end(JSON.stringify(tokenVideoIdJson));
+
+			});
+
+
+			
+	 	   
+		});	
+
+		
+	}).catch((error) => {
+		console.log(error);
+	});	
+
+
+
+
+});
 
 
 app.post('/', (req, res) => {
@@ -113,9 +198,9 @@ app.post('/', (req, res) => {
 });
 
 
-//testing on 3004
-app.listen(3004, () =>
-  console.log('Example app listening on port 3004!'),
+//testing on 3021
+app.listen(3021, () =>
+  console.log('Example app listening on port 3021!'),
 );
 process.on('uncaughtException', function(err) {
     // handle the error safely
